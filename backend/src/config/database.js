4d7ff32a2760f3a1f4fa2644835340fc0f1e4a54
AlvesKZ <igoralvex01@gmail.com>
2026-02-23 14:50:34 -0300

    feat: configure firebase

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,6 +1,22 @@
+import admin from "firebase-admin";
+import serviceAccount from "./firebase-service-account.json" assert { type: "json" };
 
 class Database {
-  
+  static instance = null;
+
+  static connect() {
+    if (!admin.apps.length) {
+      admin.initializeApp({
+        credential: admin.credential.cert(serviceAccount),
+      });
+    }
+
+    if (!this.instance) {
+      this.instance = admin.firestore();
+    }
+
+    return this.instance;
+  }
 }
 
 export default Database;
